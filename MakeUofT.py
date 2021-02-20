dd25cb978386503cd5977c4f0a90e23ad2f3cc7f
shane-hsieh <shanehsieh@hotmail.com>
2021-02-20 13:44:20 -0500

    updated commands

--- MakeUofT.py
+++ MakeUofT.py
@@ -8,4 +8,4 @@ async def on_ready():
     general_channel = client.get_channel(812470218106470453) #put client ID here
     await general_channel.send(hackernews_module.tophn_wrapper(0))
 
-client.run('ODEwMjI3NDk1NTA4NzcwODg2.YCglKA.rJAJwMH1Pkr1NCrbo8JXHdBt6I0') #put discord bot token here
+client.run('token') #put discord bot token here
